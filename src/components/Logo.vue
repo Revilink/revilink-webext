38a28721e075d3c5eba08b7485d121c4f25806c6
selimdoyranli <selimdoyranli@gmail.com>
2024-05-24 11:37:32 +0300

    chore: init

---
+++ src/components/Logo.vue
@@ -0,0 +1,5 @@
+<template>
+  <a class="icon-btn mx-2 text-2xl" rel="noreferrer" href="https://github.com/antfu/vitesse-webext" target="_blank" title="GitHub">
+    <img src="https://revilink.io/media/core/logo/revilink_logo_512.png" alt="Revilink">
+  </a>
+</template>
---
+++ src/popup/Popup.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import { storageDemo } from '~/logic/storage'
+
+const activeLink = ref('')
+
+function formatUrl(url: string): string {
+  return encodeURIComponent(url)
+}
+
+function goExternalLink() {
+  if (activeLink.value) {
+    const formattedLink = formatUrl(activeLink.value)
+    window.open(`https://revilink.io/reviews?link=${formattedLink}`, '_blank')
+  }
+}
+
+onMounted(() => {
+  //
+})
+</script>
+
+<template>
+  <main class="w-[300px] px-4 py-5 text-center text-gray-700">
+    <Logo />
+    <div>Popup</div>
+    <SharedSubtitle />
+
+    <button class="btn mt-2" @click="goExternalLink">
+      Go to revilink
+    </button>
+    <div class="mt-2">
+      <span class="opacity-50">Storage:</span> {{ storageDemo }}
+    </div>
+    <div class="mt-2">
+      <span class="opacity-50">Active Link:</span> {{ activeLink }}
+    </div>
+  </main>
+</template>
