2896a1af41d0ed115b003a18230314bd14614e2f
selimdoyranli <selimdoyranli@gmail.com>
2024-05-25 02:24:08 +0300

    feat: ui improvements

--- src/options/Options.vue
+++ src/options/Options.vue
@@ -1,15 +1,12 @@
 <script setup lang="ts">
-import logo from '~/assets/revilink_logo_512.png'
 </script>
 
 <template>
-  <main class="px-4 py-10 text-center text-gray-700 dark:text-gray-200">
-    <img :src="logo" class="icon-btn mx-2 text-2xl" alt="extension icon">
-    <div>Options</div>
-    <SharedSubtitle />
+  <main class="px-4 py-10 text-center flex flex-col text-gray-700 dark:text-gray-200">
+    <Logo class="mx-auto" />
 
     <div class="mt-4">
-      Powered by Vite <pixelarticons-zap class="align-middle inline-block" />
+      <a href="https://revilink.io">revilink.io</a>
     </div>
   </main>
 </template>
