ed7d360ccf8dce483f958e4f4018be1b93a96c94
selimdoyranli <selimdoyranli@gmail.com>
2024-06-04 11:30:41 +0300

    refactor: revilink reviews embed

--- src/popup/Popup.vue
+++ src/popup/Popup.vue
@@ -1,22 +1,6 @@
 <script setup lang="ts">
 import type { Tabs } from 'webextension-polyfill'
-import { URL } from '~/system/constants'
-import { useApi } from '~/composables'
 import { appStorage } from '~/logic/storage'
-import noCommentIcon from '~/assets/elements/state/no_chat.svg'
-
-const review = reactive({
-  isBusy: false,
-  hasError: false,
-  items: [],
-  meta: {},
-}) as any
-
-const urlReaction = reactive({
-  isBusy: false,
-  hasError: false,
-  meta: {},
-}) as any
 
 function formatUrl(url: string): string {
   return encodeURIComponent(url)
@@ -28,72 +12,15 @@ function getReviewsLink() {
   if (url) {
     const formattedLink = formatUrl(url)
 
-    return `https://revilink.io/reviews?link=${formattedLink}`
+    return `${formattedLink}`
   }
   else {
     return 'https://revilink.io'
   }
 }
 
-async function openReviewsLink() {
-  const link = getReviewsLink()
-
-  window.open(link || 'https://revilink.io', '_blank')
-}
-
-async function fetchReviews() {
-  const { fetchReviews: _fetchReviews } = useApi()
-
-  review.isBusy = true
-
-  const url = (appStorage.value.activeTab as Tabs.Tab).url as string
-
-  if (url) {
-    const { data, meta } = await _fetchReviews({ url })
-
-    if (data && meta) {
-      review.items = data
-      review.meta = meta
-    }
-
-    review.isBusy = false
-  }
-}
-
-async function fetchUrlReactions() {
-  const { fetchUrlReactions: _fetchUrlReactions } = useApi()
-
-  urlReaction.isBusy = true
-
-  const url = (appStorage.value.activeTab as Tabs.Tab).url as string
-
-  if (url) {
-    const { meta } = await _fetchUrlReactions({ url })
-
-    if (meta)
-      urlReaction.meta = meta
-
-    urlReaction.isBusy = false
-  }
-}
-
-watch(
-  () => (appStorage.value.activeTab as Tabs.Tab).url,
-  async (value) => {
-    if (value) {
-      await fetchReviews()
-      await fetchUrlReactions()
-    }
-  },
-)
-
 onMounted(async () => {
   await nextTick()
-
-  if (appStorage.value.activeTab) {
-    await fetchReviews()
-    await fetchUrlReactions()
-  }
 })
 </script>
 
@@ -110,120 +37,12 @@ onMounted(async () => {
       </div>
     </header>
 
-    <section class="block pt-11 mb-0">
-      <!-- Url Reactions -->
-      <template v-if="urlReaction.isBusy">
-        <p class="block my-6 text-center text-gray-500">
-          Loading url reactions...
-        </p>
-      </template>
-      <template v-else-if="urlReaction.hasError">
-        <p class="block my-6 text-center text-red">
-          Fetching error on url reactions
-        </p>
-      </template>
-      <template v-else>
-        <section v-if="Object.keys(urlReaction).length > 0 && Object.keys(urlReaction.meta).length > 0" class="flex gap-5 mb-5" @click="openReviewsLink">
-          <button class="flex flex-col items-center">
-            <fluent-emoji-red-heart size="22px" />
-            <span class="mt-1 text-gray-500">{{ urlReaction.meta.reactionCount.heart }}</span>
-          </button>
-          <button class="flex flex-col items-center">
-            <fluent-emoji-bookmark size="22px" />
-            <span class="mt-1 text-gray-500">{{ urlReaction.meta.reactionCount.bookmark }}</span>
-          </button>
-          <button class="flex flex-col items-center">
-            <fluent-emoji-thumbs-down-medium-light size="22px" />
-            <span class="mt-1 text-gray-500">{{ urlReaction.meta.reactionCount.dislike }}</span>
-          </button>
-          <button class="flex flex-col items-center">
-            <fluent-emoji-grinning-face-with-smiling-eyes size="22px" />
-            <span class="mt-1 text-gray-500">{{ urlReaction.meta.reactionCount.smile }}</span>
-          </button>
-          <button class="flex flex-col items-center">
-            <fluent-emoji-astonished-face size="22px" />
-            <span class="mt-1 text-gray-500">{{ urlReaction.meta.reactionCount.wow }}</span>
-          </button>
-          <button class="flex flex-col items-center">
-            <fluent-emoji-thinking-face size="22px" />
-            <span class="mt-1 text-gray-500">{{ urlReaction.meta.reactionCount.thinking }}</span>
-          </button>
-          <button class="flex flex-col items-center">
-            <fluent-emoji-face-with-symbols-on-mouth size="22px" />
-            <span class="mt-1 text-gray-500">{{ urlReaction.meta.reactionCount.angry }}</span>
-          </button>
-        </section>
-      </template>
-
-      <!-- Reviews -->
-      <template v-if="review.isBusy">
-        <p class="block my-6 text-center text-gray-500">
-          Loading reviews...
-        </p>
-      </template>
-      <template v-else-if="review.hasError">
-        <p class="block my-6 text-center text-red">
-          Fetching error on reviews
-        </p>
-      </template>
-      <template v-else>
-        <template v-if="review.items?.length > 0">
-          <template v-if="review.meta.pagination && Object.keys(review.meta.pagination) && review.meta.pagination.total > 0">
-            <strong class="text-gray-800 text-[14px] flex"><span class="me-1">💬</span> <span class="relative top-[-2px]">Comments ({{ review.meta.pagination.total }})</span></strong>
-
-            <!-- Comments -->
-            <div v-for="item in review.items" :key="item.id" class="block mt-2">
-              <!-- Comment Card -->
-              <div class="relative bg-white border-1 border-b-bluegray rounded-lg p-4">
-                <div class="flex">
-                  <img v-if="item.user.avatar?.formats?.thumbnail?.url" :src="`${item.user.avatar.formats.thumbnail.url}`" :alt="item.user.username" class="rounded-full object-cover w-[32px] h-[32px] border-1 border-gray-200">
-                  <img v-else :src="`${URL}/media/core/user.png`" :alt="item.user.username" class="rounded-full object-cover w-[32px] h-[32px] border-1 border-gray-200">
-                  <div class="ps-2 pt-0">
-                    <strong class="block line-clamp-1">{{ item.user.username }}</strong>
-                    <timeago class="relative top-[-4px] text-[10px]" :datetime="String(item.updatedAt || item.createdAt)" />
-                  </div>
-                </div>
-                <div class="ps-10">
-                  <p class="mt-1 mb-2 break-words line-clamp-2 text-md">
-                    {{ item.content }}
-                  </p>
-                  <div class="flex gap-4 mt-3" @click="openReviewsLink">
-                    <button class="flex flex-row items-center">
-                      <ri-heart-3-line size="14px" />
-                      <span class="ms-1 text-[12px] text-gray-500">{{ item.likeCount }}</span>
-                    </button>
-                    <button class="flex flex-row items-center">
-                      <ri-chat-1-line size="14px" />
-                      <span class="ms-1 text-[12px] text-gray-500">{{ item.replyCount }}</span>
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div class="flex justify-center">
-              <a :href="getReviewsLink()" target="_blank" class="inline-flex py-1 px-6 bg-transparent hover:bg-gray-200 text-gray-900 mt-4 mx-auto rounded">
-                🔗 See more comments
-              </a>
-            </div>
-          </template>
-        </template>
-        <template v-else>
-          <div class="flex flex-col justify-center items-center">
-            <img :src="noCommentIcon" class="grayscale-[1]" width="164" alt="No comment">
-            <h4 class="text-gray-800 my-4 text-lg">
-              No comments
-            </h4>
-            <p>No comments have been made about the thing in the link yet</p>
-            <a :href="getReviewsLink()" target="_blank" class="flex btn bg-black mt-6">
-              See on Revilink
-            </a>
-          </div>
-        </template>
-      </template>
+    <section class="block pt-6 mb-0">
+      <!-- eslint-disable-next-line -->
+      <iframe id="revilink-reviews-embed-iframe" :src="`https://revilink.io/embed/reviews?link=${getReviewsLink()}&urlReactions=true&commentListHead=true&avatar=true&reply=true&like=true&commentFormAvatar=true&perPage=10&page=1`" frameborder="0" width="600" height="800" scrolling="no" allowtransparency="true"></iframe>
     </section>
 
-    <footer class="absolute left-6 bottom-4 flex flex-col text-[11px] text-gray-600 mt-10">
+    <footer class="relative left-0 bottom-4 flex flex-col text-[11px] text-gray-600 mt-10">
       <p><a href="https://revilink.io" target="_blank" class="text-gray-800">Revilink</a> - Comment on any link on the internet</p>
       <p>All rights reserved © {{ new Date().getFullYear() }}. revilink.io by <a href="https://selimdoyranli.com" target="_blank" class="text-gray-800">@selimdoyranli</a></p>
     </footer>
--- src/popup/main.ts
+++ src/popup/main.ts
@@ -8,3 +8,85 @@ const app = createApp(App)
 setupApp(app)
 app.mount('#app')
 app.use(timeago)
+
+;(function () {
+  // eslint-disable-next-line no-var
+  var applyCSS = function () {
+    const iframe = document.getElementById('revilink-reviews-embed-iframe')
+
+    if (iframe) {
+      let customStyles = ''
+      const styleElements = document.getElementsByTagName('style')
+
+      for (let i = 0; i < styleElements.length; i++)
+        customStyles += styleElements[i].textContent
+      if (customStyles) {
+        (iframe as HTMLIFrameElement).contentWindow?.postMessage(
+          {
+            type: 'applyCSS',
+            styles: customStyles,
+          },
+          '*',
+        )
+      }
+    }
+  }
+
+  const setEmbedHeight = function () {
+    window.addEventListener('message', (event) => {
+      if (event.data.type === 'revilink-reviews-embed-height') {
+        const iframe = document.getElementById(
+          'revilink-reviews-embed-iframe',
+        )
+
+        if (iframe) {
+          iframe.style.height = `${event.data.height + 32}px`
+          applyCSS()
+        }
+      }
+    })
+  }
+
+  const listenAuth = function () {
+    window.addEventListener('message', (event) => {
+      if (event.data.type === 'on-click-google-auth') {
+        const popup = window.open(
+          event.data.url,
+          'popup',
+          'width=600,height=600',
+        )
+
+        if (popup) {
+          window.addEventListener('message', (event) => {
+            if (event.data.type === 'google-callback-success') {
+              const iframe = document.getElementById(
+                'revilink-reviews-embed-iframe',
+              )
+
+              if (iframe) {
+                setTimeout(() => {
+                  // eslint-disable-next-line no-self-assign
+                  iframe.src = iframe.src
+                }, 0)
+              }
+            }
+          })
+        }
+      }
+    })
+  }
+
+  // Ensure the functions are called after the script is loaded
+  if (document.readyState === 'complete') {
+    setEmbedHeight()
+    applyCSS()
+    listenAuth()
+  }
+  else {
+    window.addEventListener('load', () => {
+      setEmbedHeight()
+      applyCSS()
+      listenAuth()
+    })
+  }
+})()
